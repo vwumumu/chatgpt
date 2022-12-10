1d6dae058a1cbe6c4e7ff63692409647564df4e5
Mumu Wu <vwumumu@gmail.com>
2022-12-10 23:06:49 +0800

    correct index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// const CLIENT_CONFIG = {
-//   client_id: "65334040-1fd0-4142-98ce-4fbd0e81aaab",
-//   client_secret: "d9dc58107xxxxxxxxxxxxxxxxxx",
-//   pin: "415447",
-//   session_id: "8ca9dc89-5d26-4dc4-8a35-5afdd027a4dc",
-//   pin_token: "tDs2MunwyNb8l_i9VDmSKgUIkIr69lIUPDgAgeUQxyA",
-//   private_key:
-//     "1TwZvIRmNGXW2wO92BoZaH5wcKMGzHcRBcOdYljP-6Biqes4_9ipMsN9v_kG2nmh-YGrxl53X2DlzcGA_4NLwQ",
-// };
-
-// const API_KEY = "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";
-
 const { CLIENT_CONFIG, API_KEY } = require('./constants');
 
 const request = require("request");
@@ -53,25 +41,23 @@ socketClient.get_message_handler = async function (message) {
     },
   };
 
-  // if (message.data.category === "PLAIN_TEXT") {
-  //   const self = this;
-  //   request(options, function (error, response, body) {
-  //     if (!error && response.statusCode == 200) {
-  //       const q = message.data.parseData;
-  //       const a = body.choices[0].text.replace(/\n[\n]+/g, "");
-  //       const result = "Q: " + q + "\n" + "A: " + a;
-  //       console.log(message.data.user_id, result);
-  //       self.send_text(result, message);
-  //     } else {
-  //       console.error(error);
-  //     }
-  //   });
-  // } else {
-  //   await this.send_text("Not text", message);
-  // }
+  if (message.data.category === "PLAIN_TEXT") {
+    const self = this;
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        const q = message.data.parseData;
+        const a = body.choices[0].text.replace(/\n[\n]+/g, "");
+        const result = "Q: " + q + "\n" + "A: " + a;
+        self.send_text(result, message);
+      } else {
+        console.error(error);
+      }
+    });
+  } else {
+    await this.send_text("Not text", message);
+  }
 
-  console.log(message.data.parseData);
-  await this.send_text('感谢您的关注，今天的能源已经耗尽了，我们明天再见！Have a good day!', message)
+  // await this.send_text('感谢您的关注，今天的能源已经耗尽了，我们明天再见！Have a good day!', message)
 };
 
 socketClient.start();
