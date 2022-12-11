28dc16800976e6c04146fcdc8dc39061630abbe3
Mumu Wu <vwumumu@gmail.com>
2022-12-11 10:15:09 +0800

    add total throttle

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+// Import Key
 const { CLIENT_CONFIG, API_KEY } = require("./constants");
 
+// Import modules
 const request = require("request");
 const { MixinSocket } = require("mixin-node-sdk");
 
+// New object
 const socketClient = new MixinSocket(CLIENT_CONFIG, true, true);
 
+// Variables
 const MODEL = "text-davinci-003";
 const API_URL = "https://api.openai.com/v1/completions";
+let counter = 3000;
 
+// Message handling
 socketClient.get_message_handler = async function (message) {
+  // Read message
   await this.read_message(message);
 
+  // Error handling
   if (
     !message.action ||
     message.action === "ACKNOWLEDGE_MESSAGE_RECEIPT" ||
@@ -23,8 +31,10 @@ socketClient.get_message_handler = async function (message) {
 
   if (message.error) return console.log(message.error);
 
+  // Get input
   const TEXT = message.data.parseData;
 
+  // Handle input
   const options = {
     method: "POST",
     url: API_URL,
@@ -41,20 +51,33 @@ socketClient.get_message_handler = async function (message) {
     },
   };
 
-  if (message.data.category === "PLAIN_TEXT") {
-    const self = this;
-    request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        const q = message.data.parseData;
-        const a = body.choices[0].text.replace(/\n[\n]+/g, "");
-        const result = "Q: " + q + "\n" + "A: " + a;
-        self.send_text(result, message);
-      } else {
-        console.error(error);
-      }
-    });
+  // Request and Output handling
+  if (counter > 0) {
+    if (message.data.category === "PLAIN_TEXT") {
+      const self = this;
+      request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          const q = message.data.parseData;
+          const a = body.choices[0].text.replace(/\n[\n]+/g, "");
+          const result = "Q: " + q + "\n" + "A: " + a;
+          self.send_text(result, message);
+        } else {
+          console.error(error);
+        }
+      });
+    } else {
+      await this.send_text("Not text", message);
+    }
+    counter -= 1;
+    this.send_text(
+      "今日我的可用总查询次数还剩余 " + counter + " 次。",
+      message
+    );
   } else {
-    await this.send_text("Not text", message);
+    await this.send_text(
+      "感谢您的关注，今天的能源已经耗尽了，我们明天再见！Have a good day!",
+      message
+    );
   }
 };
 
